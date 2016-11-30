d029ce90504e8588c0524f24fc51f0ea2232c495
Raitokomniak <raitokomniak@gmail.com>
2016-11-30 22:18:54 +0200

    Added templates for score and stage end

--- Assets/Scripts/Enemy/EnemyLib.cs
+++ Assets/Scripts/Enemy/EnemyLib.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bae45b8e3287e068a562edfcb687800b8a132f70172d172a13a06a8bda18502
-size 14027
+oid sha256:68b6a5f58aa1be200c45771930a703678f0bf81c895de69f14c99850ca308daf
+size 14036
