8915b9a93f473a081b9af937f9a56f4ced11c99b
Raitokomniak <raitokomniak@gmail.com>
2016-11-30 13:12:45 +0200

    Environment acceleration/deceleration works

--- Assets/Scripts/Environment/EnvironmentController.cs
+++ Assets/Scripts/Environment/EnvironmentController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c83721e07d100c11faf2cd83893093117505b1932f097a27defb5724d2094895
-size 793
+oid sha256:062a662627e81a321eefe18916793f5724dcfdb0b9cd9e114dff78a0f18ab67b
+size 993
