cb2b4199c14fcb92653f34f61609fe6daafe7611
Anni Toivanen <raitokomniak@gmail.com>
2022-04-27 11:45:47 +0300

    Converted into a new Unity version

--- Assets/Scripts/GameController/MenuController.cs
+++ Assets/Scripts/GameController/MenuController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:533166d50959670d742b8d70ca0131f975440f77db398d9cf4b4a55e0fd541c3
-size 3238
+oid sha256:ebbbd9fa2ac83bcce251ae4ef93c289c2d8bf440c1c01d9ccaf07fc9f0ff145e
+size 3221
--- Assets/Scripts/GameController/SceneHandler.cs
+++ Assets/Scripts/GameController/SceneHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:496b1a88c347216b5f6c0f6ae860ba6d470543885f89dca4efeb4006dc6a65f6
-size 4504
+oid sha256:ef00baafe06da8e511cb888c556499e8e9e35b3a92ee07c3949d705c90c82dac
+size 4649
