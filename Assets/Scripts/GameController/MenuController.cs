c67c1d354a063566a17f738c1460b6b3792dd46c
Anni Toivanen <raitokomniak@gmail.com>
2022-04-29 16:23:00 +0300

    Cleanup for build

--- Assets/Scripts/GameController/MenuController.cs
+++ Assets/Scripts/GameController/MenuController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0535faf676b54562b86bc5aa0e127ea674ccf9ee4fd2ea8a60f4c550dfdc4312
-size 2913
+oid sha256:ab74a2223a42146ae5cf7ba08c3cc5bac79b4d75fd001f6c4e50a034bfc3e50f
+size 2897
--- Assets/Scripts/Player/PlayerHandler.cs
+++ Assets/Scripts/Player/PlayerHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81b9e8253ca4c62d0cfc32f8435635239c17a5b80f66497ea9da3c167b9e5085
-size 2201
+oid sha256:d5e79b10ad9310a149795096d5d7e23237f3a0f96488deb32db5a6634f9ec748
+size 2200
