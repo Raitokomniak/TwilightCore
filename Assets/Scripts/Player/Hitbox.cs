506771e2955fc6125a3e912a5f8680a5b5458834
Anni Toivanen <raitokomniak@gmail.com>
2022-05-06 11:51:57 +0300

    Optimized hitbox script

--- Assets/Scripts/Player/Hitbox.cs
+++ Assets/Scripts/Player/Hitbox.cs
@@ -3,27 +3,32 @@ using System.Collections;
 
 public class Hitbox : MonoBehaviour {
 
+	PlayerHandler player;
+
+	void Awake(){
+		player = Game.control.player;
+	}
 	void OnTriggerEnter2D(Collider2D c)
 	{
 		if(c.gameObject.tag == "EnemyProjectile" || c.gameObject.tag == "Enemy")
 		{
-			if (!Game.control.player.health.invulnerable && !Game.control.stageHandler.gameOver) {
-				Game.control.player.health.TakeHit ();
+			if (!player.health.invulnerable && !Game.control.stageHandler.gameOver) {
+				player.health.TakeHit ();
 			}
 		}
 
 		//6 == pickup point layer
 		if(c.gameObject.layer == 6) { 
 			if(c.gameObject.tag == "ExpPoint") {
-				Game.control.player.GainXP(7);
+				player.GainXP(7);
 			}
 
-			if(c.gameObject.tag == "DayCorePoint" && !Game.control.player.special.specialAttack) {
-				Game.control.player.special.GainCoreCharge ("Day", 2);
+			if(c.gameObject.tag == "DayCorePoint" && !player.special.specialAttack) {
+				player.special.GainCoreCharge ("Day", 2);
 			}
 
-			if(c.gameObject.tag == "NightCorePoint" && !Game.control.player.special.specialAttack) {
-				Game.control.player.special.GainCoreCharge ("Night", 2);
+			if(c.gameObject.tag == "NightCorePoint" && !player.special.specialAttack) {
+				player.special.GainCoreCharge ("Night", 2);
 			}
 
 			Destroy(c.gameObject);
